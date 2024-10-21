ffd2d68c878193caa03f80a974e6741e911c61f6
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2024-10-21 16:20:36 +0530

    removed unecessary files and added dockerfile for base img

--- data_prep/setup_dataPrep.sh
+++ data_prep/setup_dataPrep.sh
@@ -1,11 +1,20 @@
+echo "environment setup"
+# Install system dependencies (if necessary)
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install PyTorch and NumPy at build time
+RUN pip install --no-cache-dir torch numpy
+
 # Run the data preparation script
 echo "Running data_prep.py..."
 python data_prep.py
 
 #move output to sharedDir
-echo "moving output to ${OUTPUT_DIR}/data_prep/ "
+echo "moving output to ${OUTPUT_DIR}/pre_processed_data/ "
 
-mv processed_data.pt ${OUTPUT_DIR}/data_prep/
-mv vocab_size.txt ${OUTPUT_DIR}/data_prep/
-mv string_to_int.pkl ${OUTPUT_DIR}/data_prep/
-mv int_to_string.pkl ${OUTPUT_DIR}/data_prep/
+mv processed_data.pt ${OUTPUT_DIR}/pre_processed_data/
+mv vocab_size.txt ${OUTPUT_DIR}/pre_processed_data/
+mv string_to_int.pkl ${OUTPUT_DIR}/pre_processed_data/
+mv int_to_string.pkl ${OUTPUT_DIR}/pre_processed_data/
