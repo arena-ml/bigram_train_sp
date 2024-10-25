71c54f4cfe0424d9c450497f7816b1f4a9b51273
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2024-10-25 22:17:46 +0530

    misc changes in bash scripts

--- generate_text/setup_generate.sh
+++ generate_text/setup_generate.sh
@@ -1,8 +1,5 @@
-mv  ${OUTPUT_DIR}/data_prep/int_to_string.pkl ${INPUT_DIR}/generate_text/
-mv  ${OUTPUT_DIR}/data_prep/vocab_size.txt ${INPUT_DIR}/generate_text/
-
 # Run the generate script
 echo "Running generate.py..."
 python generate.py
 
-move test_prompt.txt ${OUTPUT_DIR}/generate_text/test_prompt.txt
+move test_prompt.txt ${OUTPUT_DIR}/generated_text/test_prompt.txt
