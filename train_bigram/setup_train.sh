c737bd3afb704c09f2e3f64d4d9bada79203b421
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2024-10-25 21:39:59 +0530

    fix bash script

--- train_bigram/setup_train.sh
+++ train_bigram/setup_train.sh
@@ -5,6 +5,7 @@ python train.py
 
 mv bigram_language_model_full.pth ${INPUT_DIR}/train_bigram/pre_processed_data/generate_text/
 mv vocab_size.txt  ${INPUT_DIR}/train_bigram/pre_processed_data/generate_text/
+mv int_to_string.pkl  ${INPUT_DIR}/train_bigram/pre_processed_data/generate_text/
 
 # moving repo to output
 mv ${INPUT_DIR}/train_bigram/pre_processed_data/* ${OUTPUT_DIR}/trained_bigram/
