37df28c931cb7b882813b55210aae73d62de33f4
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2024-11-09 12:54:10 +0530

    change in input path

--- data_prep/setup_dataPrep.sh
+++ data_prep/setup_dataPrep.sh
@@ -1,12 +1,3 @@
-# echo "environment setup"
-# # Install system dependencies (if necessary)
-# RUN apt-get update && apt-get install -y --no-install-recommends \
-#     build-essential \
-#     && rm -rf /var/lib/apt/lists/*
-
-# # Install PyTorch and NumPy at build time
-# RUN pip install --no-cache-dir torch numpy
-
 # Run the data preparation script
 echo "Running data_prep.py..."
 python data_prep.py
@@ -14,11 +5,11 @@ python data_prep.py
 #move output to sharedDir
 echo "moving output to ${OUTPUT_DIR}/pre_processed_data/ "
 
-mv processed_data.pt ${INPUT_DIR}/model_repo/train_bigram/
-mv vocab_size.txt ${INPUT_DIR}/model_repo/train_bigram/
-mv string_to_int.pkl ${INPUT_DIR}/model_repo/train_bigram/
-mv int_to_string.pkl ${INPUT_DIR}/model_repo/train_bigram/
+mv processed_data.pt ${INPUT_DIR}/model_repo/bigram_entity_data/train_bigram/
+mv vocab_size.txt ${INPUT_DIR}/model_repo/bigram_entity_data/train_bigram/
+mv string_to_int.pkl ${INPUT_DIR}/model_repo/bigram_entity_data/train_bigram/
+mv int_to_string.pkl ${INPUT_DIR}/model_repo/bigram_entity_data/train_bigram/
 
 #moving only relevant part to next stage
-mv ${INPUT_DIR}/model_repo/train_bigram/ ${OUTPUT_DIR}/pre_processed_data/
-mv ${INPUT_DIR}/model_repo/generate_text/ ${OUTPUT_DIR}/pre_processed_data/
+mv ${INPUT_DIR}/model_repo/bigram_entity_data/train_bigram/ ${OUTPUT_DIR}/pre_processed_data/
+mv ${INPUT_DIR}/model_repo/bigram_entity_data/generate_text/ ${OUTPUT_DIR}/pre_processed_data/
